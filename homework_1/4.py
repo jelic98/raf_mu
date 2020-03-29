896b6488f430ea2321eeefa35372447c48bee191
Lazar Jelic <lazarjelic98@gmail.com>
2020-03-29 12:11:23 +0200

    Updated 4.py

--- homework_1/4.py
+++ homework_1/4.py
@@ -27,113 +27,175 @@ import nltk
 from nltk.tokenize import regexp_tokenize
 from nltk.corpus import stopwords
 from nltk import FreqDist
+import pickle
 from sklearn.metrics import confusion_matrix, accuracy_score
 import random
 
 nltk.download('punkt')
 nltk.download('stopwords')
 
-data_x = []
-data_y = []
-
-# Ucitavanje podataka
-print("Loading data...")
 filename = '/content/drive/My Drive/Colab Notebooks/data/twitter.csv'
-with open(filename, 'r', encoding='latin1') as f:
-    reader = csv.reader(f, delimiter=',')
-    next(reader, None)
-    for row in reader:
-        data_y.append(int(row[1]))
-        data_x.append(row[2])
-
-# Ciscenje podataka
-print("Cleaning data...")
-data_x = [html.unescape(x) for x in data_x]
-data_x = [re.sub(r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', '', x) for x in data_x]
-data_x = [re.sub(r'[^\w\s]|\d+', '', x) for x in data_x]
-data_x = [re.sub(r'\s\s+', ' ', x) for x in data_x]
-data_x = [x.strip().lower() for x in data_x]
-for c in ascii_lowercase:
-    data_x = [re.sub(c + '{3,}', c+c, x) for x in data_x]
-data_x = [regexp_tokenize(x, '\w+') for x in data_x]
-stops = set(stopwords.words('english'))
-data_x = [[w for w in x if not w in stops] for x in data_x]
-
-nb_samples = 5000
-data_x = data_x[:nb_samples]
-data_y = data_y[:nb_samples]
-
-# Racunanje BOW reprezentacije i LR metrike
-print("Calculating BOW representation and LR metric...")
+pklname = '/content/drive/My Drive/Colab Notebooks/data/twitter_100k.pkl'
+nb_samples = 100000
 nb_words = 10000
-freq = FreqDist([w for x in data_x for w in x])
-vocab, _ = zip(*freq.most_common(nb_words))
-nb_words = min(nb_words, len(vocab))
-vec_x = np.zeros((len(data_x), len(vocab)), dtype=np.float32)
-lr = np.zeros(nb_words, dtype=np.float32)
-for j, w in enumerate(vocab):
-    neg = 0
-    pos = 0
-    for i, x in enumerate(data_x):
-        cnt = x.count(w)
-        vec_x[i][j] = cnt
-        neg += cnt if data_y[i] == 0 else 0
-        pos += cnt if data_y[i] == 1 else 0
-    lr[j] = (pos / neg) if pos >= 10 and neg >= 10 else 0
-
-# Deljenje podataka na skup za treniranje i testiranje
-print("Dividing dataset...")
-train_x, test_x = np.split(vec_x, [int(len(vec_x)*0.8)])
-train_y, test_y = np.split(data_y, [int(len(data_y)*0.8)])
-nb_train = len(train_x)
-nb_test = len(test_x)
-
-# Racunanje broja pojavljivanja svake reci u svakoj klasi
-print("Calculating every word occurrence for every class...")
 nb_classes = 2
-occs = np.zeros((nb_classes, nb_words), dtype=np.float32)
-for i, y in enumerate(train_y):
-    for w in range(nb_words):
-        occs[y][w] += train_x[i][w]
-
-# Racunanje P(rec|klasa)
-print("Calculating P(word|class)...")
-like = np.zeros((nb_classes, nb_words), dtype=np.float32)
 nb_alpha = 1
-for c in range(nb_classes):
-    for w in range(nb_words):
-        up = occs[c][w] + nb_alpha
-        down = np.sum(occs[c]) + nb_words * nb_alpha
-        like[c][w] = up / down
+ds_from_cache = True
+
+class Dataset:
+    def __init__(self, filename):
+        self.data_x = []
+        self.data_y = []
+        self.load(filename)
+        self.clean()
+        self.calculate_bow_lr()
+        self.split()
+        self.calculate_occurrences()
+        self.calculate_likelihoods()
+
+    # Ucitavanje podataka
+    def load(self, filename):
+        print('Loading data...')
+
+        with open(filename, 'r', encoding='latin1') as fin:
+            reader = csv.reader(fin, delimiter=',')
+            next(reader, None)
+            for row in reader:
+                self.data_y.append(int(row[1]))
+                self.data_x.append(row[2])
+
+    # Ciscenje podataka
+    def clean(self):
+        print('Cleaning data...')
+
+        self.data_x = [html.unescape(x) for x in self.data_x]
+
+        self.data_x = [re.sub(r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', '', x) for x in self.data_x]
+        self.data_x = [re.sub(r'[^\w\s]|\d+', '', x) for x in self.data_x]
+        self.data_x = [re.sub(r'\s\s+', ' ', x) for x in self.data_x]
+        self.data_x = [x.strip().lower() for x in self.data_x]
+        for c in ascii_lowercase:
+            self.data_x = [re.sub(c + '{3,}', c+c, x) for x in self.data_x]
+        
+        self.data_x = [regexp_tokenize(x, '\w+') for x in self.data_x]
+        
+        stops = set(stopwords.words('english'))
+        self.data_x = [[w for w in x if not w in stops] for x in self.data_x]
+
+        self.data_x = self.data_x[:nb_samples]
+        self.data_y = self.data_y[:nb_samples]
+
+    def update_nb_words(self):
+        global nb_words
+        nb_words = min(nb_words, len(self.vocab))
+
+    # Racunanje BOW reprezentacije i LR metrike
+    def calculate_bow_lr(self):
+        print('Calculating BOW representation and LR metric...')
+
+        global nb_words
+
+        freq = FreqDist([w for x in self.data_x for w in x])
+        self.vocab, _ = zip(*freq.most_common(nb_words))
+        self.update_nb_words()
+        
+        self.vec_x = np.zeros((len(self.data_x), len(self.vocab)), dtype=np.float32)
+        self.lr = np.zeros(nb_words, dtype=np.float32)
+
+        for j, w in enumerate(self.vocab):
+            neg = 0
+            pos = 0
+            for i, x in enumerate(self.data_x):
+                cnt = x.count(w)
+                self.vec_x[i][j] = cnt
+                if self.data_y[i] == 0:
+                    neg += cnt
+                else:
+                    pos += cnt
+            if pos >= 10 and neg >= 10:
+                self.lr[j] = pos / neg
+            if j % 100 == 0:
+                print('[calculate_bow_lr] Word: {}/{}'.format(j, nb_words))
+
+    # Deljenje podataka na skup za treniranje i testiranje
+    def split(self):
+        print('Splitting data...')
+
+        self.train_x, self.test_x = np.split(self.vec_x, [int(len(self.vec_x)*0.8)])
+        self.train_y, self.test_y = np.split(self.data_y, [int(len(self.data_y)*0.8)])
+        
+        self.nb_train = len(self.train_x)
+        self.nb_test = len(self.test_x)
+
+    # Racunanje broja pojavljivanja svake reci u svakoj klasi
+    def calculate_occurrences(self):
+        print('Calculating every word occurrence for every class...')
+
+        global nb_words, nb_classes
+        
+        self.occs = np.zeros((nb_classes, nb_words), dtype=np.float32)
+        for i, y in enumerate(self.train_y):
+            for w in range(nb_words):
+                self.occs[y][w] += self.train_x[i][w]
+            if i % 1000 == 0:
+                print('[calculate_occurrences] Object: {}/{}'.format(i, self.nb_train))
+
+    # Racunanje P(rec|klasa)
+    def calculate_likelihoods(self):
+        print('Calculating P(word|class)...')
+
+        global nb_words, nb_classes, nb_alpha
+
+        self.like = np.zeros((nb_classes, nb_words), dtype=np.float32)
+        for c in range(nb_classes):
+            for w in range(nb_words):
+                up = self.occs[c][w] + nb_alpha
+                down = np.sum(self.occs[c]) + nb_words * nb_alpha
+                self.like[c][w] = up / down
+                if w % 1000 == 0:
+                    print('[calculate_likelihoods] Word: {}/{}'.format(w, nb_words))
+
+if ds_from_cache:
+    with open(pklname, 'rb') as pkl:
+        print('Using cached dataset...')
+        ds = pickle.load(pkl)
+        ds.update_nb_words()
+else:
+    with open(pklname, 'wb') as pkl:
+        print('Using new dataset...')
+        ds = Dataset(filename)
+        print('Serializing new dataset...')
+        pickle.dump(ds, pkl, pickle.HIGHEST_PROTOCOL)
 
 # Racunanje P(klasa|test)
-print("Calculating P(class|test)...")
+print('Calculating P(class|test)...')
 hyps = []
 acts = []
-priors = np.bincount(train_y) / nb_samples
+priors = np.bincount(ds.train_y) / nb_samples
 class_trans = {'Negative': 0, 'Positive': 1, 0: 'Negative', 1: 'Positive'}
-for i, x in enumerate(test_x):
+for i, x in enumerate(ds.test_x):
     probs = np.zeros(nb_classes)
     for c in range(nb_classes):
         prob = np.log(priors[c])
         for w in range(nb_words):
-            prob += x[w] * np.log(like[c][w])
+            prob += x[w] * np.log(ds.like[c][w])
         probs[c] = prob
     hyp_val = np.argmax(probs)
-    act_val = test_y[i]
+    act_val = ds.test_y[i]
     match = hyp_val == act_val
     hyps.append(class_trans[hyp_val])
     acts.append(class_trans[act_val])
-    if i % 10 == 0:
+    if i % 100 == 0:
         print('{}/{} Predicted: {} Actual: {} Match: {}'
-        .format(i+1, nb_test, class_trans[hyp_val], class_trans[act_val], match))
+        .format(i+1, ds.nb_test, class_trans[hyp_val], class_trans[act_val], match))
 
 # Racunanje tacnosti modela
-accuracy_score(acts, hyps)
-print('Accuracy:', accuracy)
+cm = confusion_matrix(acts, hyps)
+tn, _, _, tp = cm.ravel()
+acc = (tn + tp) / ds.nb_test
+print('Accuracy:', acc)
 
 # Prikazivanje matrice konfuzije
-cm = confusion_matrix(acts, hyps)
 plt.matshow(cm)
 plt.colorbar()
 plt.xlabel('Hypothesis')
@@ -141,33 +203,33 @@ plt.ylabel('Actual')
 plt.show()
 
 # Pronalazenje pet najcesce koriscenih reci u negativnim tvitovima
-freq_neg = FreqDist([w for i, x in enumerate(data_x) for w in x if data_y[i] == 0])
+freq_neg = FreqDist([w for i, x in enumerate(ds.data_x) for w in x if ds.data_y[i] == 0])
 top_neg, _ = zip(*freq_neg.most_common(5))
 print('Top negative:', top_neg)
 
 # Pronalazenje pet najcesce koriscenih reci u pozitivnim tvitovima
-freq_pos = FreqDist([w for i, x in enumerate(data_x) for w in x if data_y[i] == 1])
+freq_pos = FreqDist([w for i, x in enumerate(ds.data_x) for w in x if ds.data_y[i] == 1])
 top_pos, _ = zip(*freq_pos.most_common(5))
 print('Top positive:', top_pos)
 
 # Pronalazenje pet reci sa najmanjom vrednoscu LR metrike
 lr_min = []
 min_cnt = 1
-for i in lr.argsort():
+for i in ds.lr.argsort():
     if min_cnt > 5:
         break
-    if lr[i] > 0:
+    if ds.lr[i] > 0:
         lr_min.append(i)
         min_cnt += 1
-print('LR lowest:', [vocab[x] for x in lr_min])
+print('LR lowest:', [ds.vocab[x] for x in lr_min])
 
 # Pronalazenje pet reci sa najvecom vrednoscu LR metrike
 lr_max = []
 max_cnt = 1
-for i in (-lr).argsort():
+for i in (-ds.lr).argsort():
     if max_cnt > 5:
         break
-    if lr[i] > 0:
+    if ds.lr[i] > 0:
         lr_max.append(i)
         max_cnt += 1
-print('LR highest:', [vocab[x] for x in lr_max])
+print('LR highest:', [ds.vocab[x] for x in lr_max])
