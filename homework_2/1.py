e2477a826acb63771536b267e4bcd159c893ac28
Lazar Jelic <lazarjelic98@gmail.com>
2020-05-25 14:35:21 +0200

    Finished first task

--- homework_2/1.py
+++ homework_2/1.py
@@ -14,38 +14,36 @@ epoch_max = 10
 alpha_max = 0.05
 alpha_min = 0.001
 
-window_size = 1
 num_steps = 30
-batch_size = 64
-
-lstm_size = 128
-num_layers = 1
+window_size = 1
+batch_size = 32
+lstm_size = 64
 
 test_ratio = 0.1
-date_interval = 7
 
 # Ucitavanje podataka
 csv = pd.read_csv('data/sp500.csv')
 dates, data = csv['Date'].values, csv['Close'].values
 
+nb_samples = len(data)
+
 # Konverzija datuma
 dates = [dt.datetime.strptime(d, '%Y-%m-%d').date() for d in dates]
 dates = [dates[i + num_steps] for i in range(nb_samples - num_steps)]
 
-nb_samples = len(data)
-
-# Grupisanje podataka (window_size)
+# Grupisanje podataka pomocu kliznog prozora
 data = [data[i * window_size : (i + 1) * window_size] for i in range(nb_samples // window_size)]
 
 # Normalizacija podataka
 norm = [data[0][0]] + [data[i-1][-1] for i, _ in enumerate(data[1:])]
-data = [curr / norm[i] for i, curr in enumerate(data)]
+data = [curr / norm[i] - 1.0 for i, curr in enumerate(data)]
 
-# Grupisanje podataka (num_steps)
+# Grupisanje podataka za propagaciju greske kroz vreme
 x = [data[i : i + num_steps] for i in range(nb_samples - num_steps)]
 y = [data[i + num_steps] for i in range(nb_samples - num_steps)]
 
 nb_train = int(len(x) * (1.0 - test_ratio))
+nb_test = nb_samples - nb_train - num_steps
 nb_batches = math.ceil(nb_train / batch_size)
 
 # Skup podataka za treniranje
@@ -60,7 +58,6 @@ test_y = y[nb_train:]
 norm_y = [norm[i + num_steps] for i in range(nb_samples - num_steps)]
 norm_test_y = norm_y[nb_train:]
 
-# Konstrukcija grafa
 tf.reset_default_graph()
 
 # Cene tokom prethodnih dana
@@ -72,20 +69,28 @@ Y = tf.placeholder(tf.float32, [None, window_size])
 # Stopa ucenja
 L = tf.placeholder(tf.float32, None)
 
+# LSTM sloj
 lstm = [tf.contrib.rnn.LSTMCell(lstm_size, state_is_tuple=True)]
 rnn = tf.contrib.rnn.MultiRNNCell(lstm, state_is_tuple=True)
-        
+
+# Izlaz LSTM sloja
 val, _ = tf.nn.dynamic_rnn(rnn, X, dtype=tf.float32)
 val = tf.transpose(val, [1, 0, 2])
 
+# Poslednji izlaz LSTM sloja
 last = tf.gather(val, val.get_shape()[0] - 1)
 
+# Obucavajuci parametri
 weight = tf.Variable(tf.truncated_normal([lstm_size, window_size]))
 bias = tf.Variable(tf.constant(0.1, shape=[window_size]))
 
+# Predvidjena cena
 prediction = tf.add(tf.matmul(last, weight), bias)
 
+# MSE za predikciju
 loss = tf.reduce_mean(tf.square(tf.subtract(prediction, Y)))
+
+# Gradijentni spust pomocu Adam optimizacije
 optimizer = tf.train.AdamOptimizer(L).minimize(loss)
 
 with tf.Session() as sess:
@@ -110,14 +115,18 @@ with tf.Session() as sess:
     # Testiranje modela
     test_pred = sess.run(prediction, {X: test_x, Y: test_y, L: alpha})
 
+    # Tacnost modela za predikciju monotonosti fluktuacije cene
+    acc = sum(1 for i in range(nb_test) if test_pred[i] * test_y[i] > 0) / nb_test
+    print('Accuracy: {}'.format(acc))
+
     # Denormalizacija podataka
-    test_y = [curr * norm_test_y[i] for i, curr in enumerate(test_y)]
-    test_pred = [curr * norm_test_y[i] for i, curr in enumerate(test_pred)]
+    test_y = [(curr + 1.0) * norm_test_y[i] for i, curr in enumerate(test_y)]
+    test_pred = [(curr + 1.0) * norm_test_y[i] for i, curr in enumerate(test_pred)]
 
     # Prikazivanje predikcija
     plt.figure(figsize=(16,4))
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=date_interval))
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=nb_test//20))
     plt.plot(dates[nb_train:], test_y, '-b', label='Actual')
     plt.plot(dates[nb_train:], test_pred, '--r', label='Predicted')
     plt.gcf().autofmt_xdate()
