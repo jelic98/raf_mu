b72fc507d0bf0cf188ed5bcd7dbd5529164a29cf
Lazar Jelic <lazarjelic98@gmail.com>
2020-05-27 23:47:42 +0200

    Update 2.py

--- homework_2/2.py
+++ homework_2/2.py
@@ -14,17 +14,14 @@ warnings.filterwarnings('ignore')
 
 # Hiperparametri
 epoch_max = 10
-alpha_max = 0.05
+alpha_max = 0.025
 alpha_min = 0.001
-
-num_steps = 30
-window_size = 1
 batch_size = 32
-lstm_size = 64
-
-embed_size = 2
-
+window_size = 7
 test_ratio = 0.1
+max_time = 16
+lstm_size = 64
+embed_size = 2
 
 # Simboli akcija
 train_symbols, test_symbols = None, ['AAPL', 'GOOG']
@@ -52,74 +49,73 @@ for sym_index, sym in enumerate(train_symbols):
     csv = pd.read_csv('data/sp500_{}.csv'.format(sym))
     dts, data = csv['Date'].values, csv['Close'].values
 
-    nb_samples = len(data)
-
     # Konverzija datuma
     dts = [dt.datetime.strptime(d, '%Y-%m-%d').date() for d in dts]
-    dates.append([dts[i + num_steps] for i in range(nb_samples - num_steps)])
+    dates.append([dts[i + max_time] for i in range(len(dts) - max_time)])
 
     # Grupisanje podataka pomocu kliznog prozora
-    data = [data[i * window_size : (i + 1) * window_size] for i in range(nb_samples // window_size)]
+    data = [data[i : i + window_size] for i in range(len(data) - window_size)]
 
     # Normalizacija podataka
     norm = [data[0][0]] + [data[i-1][-1] for i, _ in enumerate(data[1:])]
     data = [curr / norm[i] - 1.0 for i, curr in enumerate(data)]
 
-    # Grupisanje podataka za propagaciju greske kroz vreme
-    x = [data[i : i + num_steps] for i in range(nb_samples - num_steps)]
-    y = [data[i + num_steps] for i in range(nb_samples - num_steps)]
-
-    nb_train.append(int(len(x) * (1.0 - test_ratio)))
-    nb_test.append(nb_samples - nb_train[sym_index] - num_steps)
+    nb_samples = len(data) - max_time
+    nb_train.append(int(nb_samples * (1.0 - test_ratio)))
+    nb_test.append(nb_samples - nb_train[sym_index])
     nb_batches.append(math.ceil(nb_train[sym_index] / batch_size))
 
+    # Grupisanje podataka za propagaciju greske kroz vreme
+    x = [data[i : i + max_time] for i in range(nb_samples)]
+    y = [data[i + max_time][-1] for i in range(nb_samples)]
+
     # Skup podataka za treniranje
-    train_x.append([x[b * batch_size : (b + 1) * batch_size] for b in range(nb_batches[sym_index])])
-    train_y.append([y[b * batch_size : (b + 1) * batch_size] for b in range(nb_batches[sym_index])])
+    train_x.append([x[i : i + batch_size] for i in range(0, nb_train[sym_index], batch_size)])
+    train_y.append([y[i : i + batch_size] for i in range(0, nb_train[sym_index], batch_size)])
 
     # Skup podataka za testiranje
-    test_x.append(x[nb_train[sym_index]:])
-    test_y.append(y[nb_train[sym_index]:])
+    test_x.append(x[-nb_test[sym_index]:])
+    test_y.append(y[-nb_test[sym_index]:])
 
     # Skup podataka za denormalizaciju
-    norm_y = [norm[i + num_steps] for i in range(nb_samples - num_steps)]
-    norm_test_y.append(norm_y[nb_train[sym_index]:])
+    norm_y = [norm[i + max_time] for i in range(nb_samples)]
+    norm_test_y.append(norm_y[-nb_test[sym_index]:])
 
 tf.reset_default_graph()
 
 # Cene tokom prethodnih dana
-X = tf.placeholder(tf.float32, [None, num_steps, window_size])
+X = tf.placeholder(tf.float32, [None, max_time, window_size])
 
 # Cena na trenutni dan
-Y = tf.placeholder(tf.float32, [None, window_size])
+Y = tf.placeholder(tf.float32, [None])
 
 # Stopa ucenja
-L = tf.placeholder(tf.float32, None)
+L = tf.placeholder(tf.float32)
 
 # Celobrojna reprezentacija simbola akcija
 S = tf.placeholder(tf.int32, [None, 1])
 
 # Embedovanje simbola akcija
-embed_matrix = tf.Variable(tf.random_uniform([nb_stocks, embed_size], -1.0, 1.0))
-stack_symbols = tf.tile(S, [1, num_steps])
-stack_embeds = tf.nn.embedding_lookup(embed_matrix, stack_symbols)
+embeds = tf.Variable(tf.random_uniform([nb_stocks, embed_size], maxval=1.0))
+sym_tiles = tf.tile(S, [1, max_time])
+lookup = tf.nn.embedding_lookup(embeds, sym_tiles)
 
 # Spajanje cena tokom prethodnih dana sa embedingom simbola akcija
-inputs = tf.concat([X, stack_embeds], axis=2)
+x_embed = tf.concat([X, lookup], axis=2)
 
 # LSTM sloj
 rnn = tf.contrib.rnn.MultiRNNCell([tf.contrib.rnn.LSTMCell(lstm_size)])
 
 # Izlaz LSTM sloja
-val, _ = tf.nn.dynamic_rnn(rnn, inputs, dtype=tf.float32)
+val, _ = tf.nn.dynamic_rnn(rnn, x_embed, dtype=tf.float32)
 val = tf.transpose(val, [1, 0, 2])
 
 # Poslednji izlaz LSTM sloja
 last = tf.gather(val, val.get_shape()[0] - 1)
 
 # Obucavajuci parametri
-weight = tf.Variable(tf.truncated_normal([lstm_size, window_size]))
-bias = tf.Variable(tf.constant(0.1, shape=[window_size]))
+weight = tf.Variable(tf.random_normal([lstm_size, 1]))
+bias = tf.Variable(tf.constant(0.0, shape=[1]))
 
 # Predvidjena cena
 prediction = tf.add(tf.matmul(last, weight), bias)
@@ -135,10 +131,8 @@ with tf.Session() as sess:
 
     # Treniranje modela
     for epoch in range(epoch_max):
-        epoch_loss = 0
-
         # Adaptiranje stope ucenja
-        alpha = max(alpha_min, alpha_max * (1 - epoch / epoch_max))
+        epoch_loss, alpha = 0, max(alpha_min, alpha_max * (1 - epoch / epoch_max))
 
         # Treniranje modela za odredjene akcije
         for sym_index, sym in enumerate(train_symbols):
@@ -150,9 +144,6 @@ with tf.Session() as sess:
                 loss_val, _ = sess.run([loss, optimizer], feed)
                 epoch_loss += loss_val
 
-            print('Loss ({}): {}'.format(sym, epoch_loss))
-
-        epoch_loss /= epoch_max
         print('Epoch: {}/{}\tLoss: {}'.format(epoch+1, epoch_max, epoch_loss))
 
     # Testiranje modela za odredjene akcije
@@ -167,15 +158,15 @@ with tf.Session() as sess:
         print('Accuracy ({}): {}'.format(sym, acc))
 
         # Denormalizacija podataka
-        test_y[sym_index] = [(curr + 1.0) * norm_test_y[sym_index][i] for i, curr in enumerate(test_y[sym_index])]
-        test_pred = [(curr + 1.0) * norm_test_y[sym_index][i] for i, curr in enumerate(test_pred)]
+        denorm_y = [(curr + 1.0) * norm_test_y[sym_index][i] for i, curr in enumerate(test_y[sym_index])]
+        denorm_pred = [(curr + 1.0) * norm_test_y[sym_index][i] for i, curr in enumerate(test_pred)]
 
         # Prikazivanje predikcija
         plt.figure(figsize=(16,4))
         plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-        plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=nb_test[sym_index]//16))
-        plt.plot(dates[sym_index][nb_train[sym_index]:], test_y[sym_index], '-b', label='Actual')
-        plt.plot(dates[sym_index][nb_train[sym_index]:], test_pred, '--r', label='Predicted')
+        plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=7))
+        plt.plot(dates[sym_index][-nb_test[sym_index]:], denorm_y, '-b', label='Actual')
+        plt.plot(dates[sym_index][-nb_test[sym_index]:], denorm_pred, '--r', label='Predicted')
         plt.gcf().autofmt_xdate()
         plt.title(sym)
         plt.legend()
